7b44da3868e2f077d3a6e5641ed847dff5396b7b
Manmohan Kumar <manmohankumar@Manmohans-MacBook-Pro.local>
2023-10-05 12:51:04 +0530

    fetch

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // images: {
-  //   domains: [
-  //     "tak.haroth.com", "nextjspics.s3.ap-south-1.amazonaws.com", "img.haroth.com"
-  //   ]
-  // },
+  images: {
+    unoptimized: true,
+    domains: [
+      "tak.haroth.com", "nextjspics.s3.ap-south-1.amazonaws.com", "img.haroth.com"
+    ]
+  },
 }
 
 module.exports = nextConfig

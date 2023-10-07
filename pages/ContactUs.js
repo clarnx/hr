d634359edf50a501950b99dea856dbe29be69a91
Manmohan Kumar <manmohankumar@Manmohans-MacBook-Pro.local>
2023-10-07 11:00:27 +0530

    contact update

--- pages/ContactUs.js
+++ pages/ContactUs.js
@@ -8,6 +8,7 @@ import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 import {BulkFormInput} from "../components/BulkPurchaseForm"
+import Link from 'next/link';
 
 
 
@@ -174,7 +175,7 @@ const ContactUs = () => {
               </svg>
               <div>
                 <h3 className="font-semibold text-gray-800">Knowledgebase</h3>
-                <p className="mt-1 text-sm text-gray-500">We are here to help with any questions or code.</p>
+                <p className="mt-1 text-sm text-gray-500">Were here to help with any questions or code.</p>
                 <a className="mt-2 inline-flex items-center gap-x-2 text-sm font-medium text-gray-600 hover:text-red-600" href="#" 
                 onClick={openBotsonicWidget} >
                   Contact support
@@ -195,8 +196,10 @@ const ContactUs = () => {
               <div>
                 <h3 className="font-semibold text-gray-800">FAQ</h3>
                 <p className="mt-1 text-sm text-gray-500">Search our FAQ for answers to anything you might ask.</p>
-                <a className="mt-2 inline-flex items-center gap-x-2 text-sm font-medium text-gray-600 hover:text-gray-800" href="/policies">
+                <a className="mt-2 inline-flex items-center gap-x-2 text-sm font-medium text-gray-600 hover:text-gray-800">
+                  <Link href={"/policies"} target='_blank'>
                   Visit FAQ
+                  </Link>
                   <svg className="w-2.5 h-2.5 transition ease-in-out group-hover:translate-x-1" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     {/* <path fill-rule="evenodd" clip-rule="evenodd" d="M0.975821 6.92249C0.43689 6.92249 -3.50468e-07 7.34222 -3.27835e-07 7.85999C-3.05203e-07 8.37775 0.43689 8.79749 0.975821 8.79749L12.7694 8.79748L7.60447 13.7596C7.22339 14.1257 7.22339 14.7193 7.60447 15.0854C7.98555 15.4515 8.60341 15.4515 8.98449 15.0854L15.6427 8.68862C16.1191 8.23098 16.1191 7.48899 15.6427 7.03134L8.98449 0.634573C8.60341 0.268455 7.98555 0.268456 7.60447 0.634573C7.22339 1.00069 7.22339 1.59428 7.60447 1.9604L12.7694 6.92248L0.975821 6.92249Z" fill="currentColor"/> */}
                   </svg>

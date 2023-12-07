bc0c08c8a1a949089e17c1147d377834ab8f0d79
Manmohan Kumar <manmohankumar@Manmohans-MacBook-Pro.local>
2023-12-07 12:35:59 +0530

    next.config.js update

--- next.config.js
+++ next.config.js
@@ -7,20 +7,22 @@ const nextConfig = {
     domains: [
       "tak.haroth.com", "nextjspics.s3.ap-south-1.amazonaws.com", "img.haroth.com", "cdn.haroth.com"
     ],
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.externals = {
-        onesignal: 'OneSignal',
-      };
-    }
-
-    return config;
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
-// Export the merged configuration
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.externals = {
+//         onesignal: 'OneSignal',
+//       };
+//     }
+
+//     return config;
+//   },
+// };
+
 module.exports = nextConfig;
